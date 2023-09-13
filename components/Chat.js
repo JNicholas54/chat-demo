355ac5086488a47826efa0320bd04720a34ae715
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-09-13 13:41:29 -0700

    adding colors and style + add media file

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,61 @@
 import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
-  const { name } = route.params;
+  const { name, color } = route.params;
+  //creating messages state
+  const [messages, setMessages] = useState();
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer :D',
+        createAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Ready to chat?!</Text>
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === 'android' ? (
+        <KeyboardAvoidingView behavior='height' />
+      ) : null}
     </View>
   );
 };
@@ -18,8 +63,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
--- components/Start.js
+++ components/Start.js
@@ -1,30 +1,69 @@
 import { useState } from 'react';
 import {
+  ImageBackground,
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
+
+const image = require('../media/images/background-image.png');
+
+const backgroundColors = {
+  a: '#DBCFB0',
+  b: '#BFC8AD',
+  c: '#90B494',
+  d: '#718F94',
+};
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');
+  const [color, setColor] = useState(backgroundColors.d);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.appTitle}>Hi, Lets get started!</Text>
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder='Type your username here to begin chatting!'
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Chat', { name: name })}
-      >
-        <Text>Start chatting here!</Text>
-      </TouchableOpacity>
+      <ImageBackground source={image} resizeMode='cover' style={styles.image}>
+        <Text style={styles.appTitle}>App title</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={setName}
+            placeholder='Type your username here'
+            placeholderTextColor='#BFC8AD'
+          />
+          <Text style={styles.colorSelector}>Choose background color:</Text>
+          <View style={styles.selectColorElement}>
+            <TouchableOpacity
+              style={[styles.circle, { backgroundColor: backgroundColors.a }]}
+              onPress={() => setColor(backgroundColors.a)}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.circle, { backgroundColor: backgroundColors.b }]}
+              onPress={() => setColor(backgroundColors.b)}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.circle, { backgroundColor: backgroundColors.c }]}
+              onPress={() => setColor(backgroundColors.c)}
+            ></TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.circle, { backgroundColor: backgroundColors.d }]}
+              onPress={() => setColor(backgroundColors.d)}
+            ></TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() =>
+              navigation.navigate('Chat', { name: name, color: color })
+            }
+          >
+            <Text style={styles.buttonText}>Start chatting</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -32,28 +71,60 @@ const Start = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: '6%',
   },
   appTitle: {
+    flex: 2,
     fontSize: 45,
     fontWeight: '600',
-    color: '#000',
+    color: '#FFFFFF',
+    alignSelf: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: '#000000',
+    padding: '6%',
   },
   textInput: {
-    width: '88%',
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: '300',
-    color: '#757083',
+    color: '#BFC8AD',
     padding: 15,
     borderWidth: 1,
+    borderColor: '#BFC8AD',
     marginTop: 15,
     marginBottom: 15,
   },
+  selectColorElement: {
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+  colorSelector: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#90B494',
+  },
+  circle: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginTop: 10,
+    marginBottom: 10,
+  },
   button: {
-    alignContent: 'center',
-    backgroundColor: '#DDDDDD',
+    alignItems: 'center',
+    backgroundColor: '#BFC8AD',
     padding: 10,
+  },
+  buttonText: {
+    color: '#000', // Text color for the button
+    fontSize: 18, // Font size
+    fontWeight: 'bold',
   },
 });
 
